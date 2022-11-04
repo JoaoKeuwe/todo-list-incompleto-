ec354cdf5d52fdf327929817aae72bd1ff5a403e
joaokeuwe <joaov2872@hotmail.com>
2022-11-04 14:54:35 -0300

    refactor: :fire: refatorando main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,15 @@
 import { createApp } from "vue";
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 import App from "./App.vue";
 import router from "./router";
-import Vue from 'vue'
+import Vue from "vue";
 
 import store from "./store/index";
 
-
-
 const app = new Vue({
-    store,
-    router,
-    render: (createEelement) => createEelement(App)
-})
+  store,
+  router,
+  render: (createEelement) => createEelement(App),
+});
 
-app.$mount("#app")
-
-  
+app.$mount("#app");
