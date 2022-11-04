559a30cd2b67a5965d48dae1d4bfd3ac3240ecd1
joaokeuwe <joaov2872@hotmail.com>
2022-11-04 14:55:34 -0300

    refactor: :fire: refatorando index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,26 +1,25 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import tasks from './tasks-list/tasks'
+import Vue from "vue";
+import Vuex from "vuex";
+import tasks from "./tasks-list/tasks";
 
 // Create a new store instance.
-Vue.use(Vuex)
+Vue.use(Vuex);
 export default new Vuex.Store({
   modules: {
-    tasks
+    tasks,
   },
-  
-  state () {
-    return {
-      count: 10
-    }
-  },
-  
-  mutations: {
-    increment (state) {
-      state.count++
-    }
-  }
 
-})
+  state() {
+    return {
+      count: 10,
+    };
+  },
+
+  mutations: {
+    increment(state) {
+      state.count++;
+    },
+  },
+});
 
 // Install the store instance as a plugin
